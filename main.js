235531c9e3841c1c10433f84cc10b7d446549d92
Meloolem <127792755+Meloolem@users.noreply.github.com>
2023-03-16 21:28:37 +0200

    Add files via upload

--- main.js
+++ main.js
@@ -21,6 +21,12 @@ document.querySelector("#button-random").onclick = function(){
     let uGDIimg = ['./img/GDI/CNCRiv_Riflemen.webp','./img/GDI/CNCRiv_Missile_Squad.webp','./img/GDI/CNCRiv_MG_Squad.webp','./img/GDI/CNCRiv_Jump_Jet_Troopers.webp','./img/GDI/CNCRiv_Shockwave_Troopers.webp','./img/GDI/CNCRiv_Sniper_Team.webp','./img/GDI/CNCRiv_Grenadier.webp','./img/GDI/CNCRiv_War_Dogs.webp','./img/GDI/CNCRiv_Rhino.webp','./img/GDI/CNCRiv_M.S.V..webp','./img/GDI/CNCRiv_Pitbull.webp','./img/GDI/CNCRiv_Shatterer.webp','./img/GDI/CNCRiv_Slingshot.webp','./img/GDI/CNCRiv_M.L.R.S..webp','./img/GDI/CNCRiv_Predator_Tank.webp','./img/GDI/CNCRiv_A.P.C..webp','./img/GDI/CNCRiv_Battering_Ram.webp','./img/GDI/CNCRiv_War_Dogs.webp','./img/GDI/CNCRiv_Talon.webp','./img/GDI/CNCRiv_Mohawk_Gunship.webp','./img/GDI/CNCRiv_Razorback.webp','./img/GDI/CNCRiv_Orca.webp','./img/GDI/CNCRiv_Hammerhead.webp','./img/GDI/CNCRiv_Orca_Bomber.webp','./img/GDI/CNCRiv_Wolverine.webp','./img/GDI/CNCRiv_Disruptor.webp','./img/GDI/CNCRiv_Juggernaut.webp','./img/GDI/CNCRiv_Zone_Trooper.webp','./img/GDI/CNCRiv_Kodiak.webp','./img/GDI/CNCRiv_Sandstorm.webp','./img/GDI/CNCRiv_Titan.webp','./img/GDI/CNCRiv_Mammoth_Tank.webp'];
     let uNODimg = ['./img/NOD/CNCRiv_Militant.webp', './img/NOD/CNCRiv_Laser_Squad.webp', './img/NOD/CNCRiv_Fanatic.webp', './img/NOD/CNCRiv_Scavenger.webp', './img/NOD/CNCRiv_Flame_Troopers.webp', './img/NOD/CNCRiv_Scarabs.webp','./img/NOD/CNCRiv_Chemical_Warriors.webp','./img/NOD/CNCRiv_Mutant_Marauder.webp','./img/NOD/CNCRiv_Cyberwheel.webp','./img/NOD/CNCRiv_Attack_Bikes.webp','./img/NOD/CNCRiv_Buggy.webp','./img/NOD/CNCRiv_Chem_Buggy.webp','./img/NOD/CNCRiv_Scorpion_Tank.webp','./img/NOD/CNCRiv_Tick_Tank.webp','./img/NOD/CNCRiv_Giga-Cannon.webp','./img/NOD/CNCRiv_Stealth_Tank.webp','./img/NOD/CNCRiv_Laser_Drone.webp','./img/NOD/CNCRiv_Banshee.webp','./img/NOD/CNCRiv_Venom.webp','./img/NOD/CNCRiv_Catalyst_Gunship.webp','./img/NOD/CNCRiv_Shade.webp','./img/NOD/CNCRiv_Inferno.webp','./img/NOD/CNCRiv_Phantom.webp','./img/NOD/CNCRiv_Widowmaker.webp','./img/NOD/CNCRiv_Flame_Tank.webp','./img/NOD/CNCRiv_Centurion.webp','./img/NOD/CNCRiv_Confessor.webp','./img/NOD/CNCRiv_Artillery.webp','./img/NOD/CNCRiv_Basilisk.webp','./img/NOD/CNCRiv_Rockworm.webp','./img/NOD/CNCRiv_Cyborg.webp',"./img/NOD/CNCRiv_Avatar.webp"];
 
+    let arrIndex = [];
+    for(let i = random(0,31); arrIndex.length < 6; i = random(0,31)){
+      if(!arrIndex.includes(i)) {arrIndex.push(i)}
+    }
+    arrIndex.sort((a, b) => a - b)
+
     if((random(0, 1)) == 0){fraction = 'ГСБ'} 
     else{fraction = 'НОД'}
 
@@ -33,19 +39,20 @@ document.querySelector("#button-random").onclick = function(){
       commander.src = сNODimg[random(0,3)]
     }
 
+
     if(fraction == 'ГСБ') {
-      unit1.src = uGDIimg.splice([random(0,31)],1);
-      unit2.src = uGDIimg.splice([random(0,30)],1);
-      unit3.src = uGDIimg.splice([random(0,29)],1);
-      unit4.src = uGDIimg.splice([random(0,28)],1);
-      unit5.src = uGDIimg.splice([random(0,27)],1);
-      unit6.src = uGDIimg.splice([random(0,26)],1);
+      unit1.src = uGDIimg[arrIndex[0]];
+      unit2.src = uGDIimg[arrIndex[1]];
+      unit3.src = uGDIimg[arrIndex[2]];
+      unit4.src = uGDIimg[arrIndex[3]];
+      unit5.src = uGDIimg[arrIndex[4]];
+      unit6.src = uGDIimg[arrIndex[5]];
     } else{
-      unit1.src = uNODimg.splice([random(0,31)],1);
-      unit2.src = uNODimg.splice([random(0,30)],1);
-      unit3.src = uNODimg.splice([random(0,29)],1);
-      unit4.src = uNODimg.splice([random(0,28)],1);
-      unit5.src = uNODimg.splice([random(0,27)],1);
-      unit6.src = uNODimg.splice([random(0,26)],1);
+      unit1.src = uNODimg[arrIndex[0]];
+      unit2.src = uNODimg[arrIndex[1]];
+      unit3.src = uNODimg[arrIndex[2]];
+      unit4.src = uNODimg[arrIndex[3]];
+      unit5.src = uNODimg[arrIndex[4]];
+      unit6.src = uNODimg[arrIndex[5]];
     }
   }
